eb6fb96c1a240f9b9cda77aa33a8a80d6f864807
Rank1AltAccount <114690080+Rank1AltAccount@users.noreply.github.com>
2022-09-29 23:25:42 +0500

    Create Find the odd int.cpp

---
+++ C++/Codewars/Find the odd int.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+int findOdd(const std::vector<int>& numbers) {
+  int count = 1;
+  int ret = 0;
+  
+  std::vector<int> newarr = numbers;
+  std::sort(newarr.begin(), newarr.end());
+ 
+  for (int i = 0; i < newarr.size(); i++) { 
+    if (newarr[i] == newarr[i + 1]) { 
+      count++;
+    }
+    else {
+      if (count % 2 != 0) {
+        ret = newarr[i];
+      }
+      else {
+        count = 1;
+      }
+    }
+  }
+      
+  return ret;
+}
